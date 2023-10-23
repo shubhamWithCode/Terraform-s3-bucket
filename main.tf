223f9171f5a3235f596005be48d954b1b4a67c77
ankit-jagtap-devops <ankujagtap20@gmail.com>
2023-10-23 22:43:53 +0530

    bucket versioning added

--- main.tf
+++ main.tf
@@ -7,3 +7,10 @@ resource "aws_s3_bucket" "terraform-bucket" {
     }
   
 }
+
+resource "aws_s3_bucket_versioning" "my-bucket-versioning" {
+  bucket = aws_s3_bucket.terraform-bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
